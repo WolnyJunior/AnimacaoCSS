a8eb924c7dcc81f3dba277c033bc6719e13f3bdc
SorinJunior <juninhosorin@gmail.com>
2023-05-01 19:19:34 -0300

    fourth commit

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-for (num = 1; num < 11; num++) {
-    console.log("5 x ", num, " = ", 5 * num);
-}
-for (num = 1; num < 11; num++) {
-    console.log("3 x ", num, " = ", 3 * num);
-}
-
-function tabuada(tab){
-    
-    for(num=1; num < 11; num++){
-        console.log(tab, " x ", num, " = ", tab * num);
-    }
-}
-
 dia = new Date()
-console.log("Dia ",dia.getDate())
-console.log("Do Mês ",dia.getMonth())
-console.log("Do Ano ",dia.getFullYear())
-console.log("São ",dia.getHours()," Horas")
-console.log("Com ",dia.getMinutes()," Minutos")
-console.log("E ",dia.getSeconds()," Segundos")
-
-console.log(dia)
 
 const month = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
 let name = month[dia.getMonth()];
